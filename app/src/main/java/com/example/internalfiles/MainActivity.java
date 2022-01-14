49b57895403c47282edad2aed6e8f1a19c15a195
ilaishimoni <74491683+ilaishimoni@users.noreply.github.com>
2022-01-14 22:34:15 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/internalfiles/MainActivity.java
+++ app/src/main/java/com/example/internalfiles/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,54 +19,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+
+/**
+ * @author		Ilai Shimoni <ilaishimoni@gmail.com>
+ * @version	     1.0
+ * @since		03/01/22
+ * application saves data into an internal file and shows saved text on the app
+ */
+
 public class MainActivity extends AppCompatActivity {
+
     Intent si;
     EditText et;
     String line;
-    String display;
+    String strrd;
+    TextView tv;
 
+
+    /**
+     * occurs during app creation ( 1 time only )
+     * <p>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         et = (EditText) findViewById(R.id.et);
-        et.setText(display);
+        tv = (TextView) findViewById(R.id.tv);
+
+        tv.setText("");
+
     }
 
+
+    /**
+     * occurs during app start ( after creation )
+     * <p>
+     */
     @Override
     protected void onStart() {
+        super.onStart();
 
-        FileInputStream fis= null;
-        try {
-            fis = openFileInput("data.txt");
+        try{
+            FileInputStream fis= openFileInput("data.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuffer sb = new StringBuffer();
+            line = br.readLine();
+            while (line != null) {
+                sb.append(line+'\n');
+                line = br.readLine();
+            }
+            strrd=sb.toString();
+            isr.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader br = new BufferedReader(isr);
-        StringBuffer sb = new StringBuffer();
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        while (line != null) {
-            sb.append(line+'\n');
-            try {
-                line = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        display=sb.toString();
-        try {
-            isr.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        super.onStart();
+        tv.setText(strrd);
     }
 
 
@@ -81,34 +94,56 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void creds(MenuItem item) {
-        si = new Intent(this,credits.class);
-        startActivity(si);
+    /**
+     * checks which button was pressed and acts accordingly
+     * @param  item
+     * @return method either moves user to another activity or closes the app with data save
+     */
+    public boolean  onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.credits) {
+            si = new Intent(this, credits.class);
+            startActivity(si);
+            return true;
+        }
+        if (id == R.id.exit) {
+            FileOutputStream fos = null;
+            try {
+                fos = openFileOutput("data.txt", MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            try {
+                bw.write(strrd + et.getText().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finish();
+            return true;
+
+        }
+        return true;
 
     }
 
-    public void finish(MenuItem item) {
-        FileOutputStream fos = null;
-        try {
-            fos = openFileOutput("data.txt",MODE_PRIVATE);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        OutputStreamWriter osw = new OutputStreamWriter(fos);
-        BufferedWriter bw = new BufferedWriter(osw);
-        try {
-            bw.write(et.getText().toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finish();
-    }
 
+
+
+
+
+    /**
+     * saves data without closing the application
+     * @param view (button)\
+     * @return method saves data to Internal File
+     */
     public void save(View view) {
         FileOutputStream fos = null;
         try {
@@ -119,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
         OutputStreamWriter osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
         try {
-            bw.write(et.getText().toString());
+            bw.write(strrd + et.getText().toString() );
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -128,11 +163,35 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        strrd =  strrd + et.getText().toString();
+        tv.setText(strrd);
+        et.setText("");
+
 
 
     }
 
+    /**
+     * Reseting the file as well as data preserved to user
+     * @param view the view
+     */
     public void reset(View view) {
-        et.setText("");
+        tv.setText("");
+        strrd = "";
+
+        try{
+            FileOutputStream fos = openFileOutput("data.txt",MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            bw.write(strrd);
+            bw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
